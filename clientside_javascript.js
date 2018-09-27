80cb111b63d05031a2a29c79437f9446994f84a6
Andre Bradshaw <bradshaw.andre@gmail.com>
2018-09-26 20:49:28 -0400

    Create clientside_javascript.js

---
+++ clientside_javascript.js
@@ -0,0 +1,35 @@
+function tr(str){
+	return str.replace(/&/g, '_AMP_').replace(/\?/g, '_QST_').replace(/#/g, '_HSH_');
+}
+
+var fullname = document.getElementsByClassName("p-name vcard-fullname")[0].innerText; 
+
+var title = document.getElementsByClassName("user-profile-bio")[0].innerText.trim();
+
+var vcard = document.getElementsByClassName("vcard-details")[0].getElementsByTagName("li");
+
+var employer = '';
+var local = '';
+var email = '';
+var links = '';
+
+for(i=0; i<vcard.length; i++) {
+	var item = vcard[i].getAttribute("itemprop");
+	if(item == "worksFor") {
+		var employer = vcard[i].innerText.trim();
+	}
+	if(item == "homeLocation") {
+		var local = vcard[i].innerText.trim();
+	}
+	if(item == "email") {
+		var email = vcard[i].innerText.trim();
+	}
+	if(item == "url") {
+		var links = vcard[i].innerText.trim();
+	}
+}
+var comment = prompt("add a note");
+
+var jsonp = '?name=' + tr(fullname) +'&title=' + tr(title) + '&empl=' + tr(employer) + '&local=' + tr(local) + '&links=' + tr(links) +'&comment=' + tr(comment);
+
+console.log(jsonp)
